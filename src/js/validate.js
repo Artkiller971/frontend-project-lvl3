5e15e80f9194226fe0bbb0cab37bcdcb137c30b6
Anton <koshevnikod@gmail.com>
2025-08-05 21:39:48 +0300

    i18next setup

--- src/js/validate.js
+++ src/js/validate.js
@@ -2,14 +2,14 @@ import * as yup from 'yup'
 
 const schema = yup
   .string()
-  .required('Пожалуйста вставьте ссылку')
-  .url('Ссылка должна быть валидным URL')
+  .required('required')
+  .url('notValidUrl')
 
 export default (urls, url) => {
   return Promise.resolve(url)
     .then((url) => {
       if (urls.includes(url)) {
-        throw new Error('RSS уже существует')
+        throw new Error('duplicate')
       }
     })
     .then(() => schema.validate(url))
---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    validate: {
+      success: 'RSS успешно загружен',
+    },
+    feed: {
+      button: 'Просмотр',
+    },
+    errors: {
+      duplicate: 'RSS уже существует',
+      required: 'Пожалуйста вставьте ссылку',
+      notValidUrl: 'Ссылка должна быть валидным URL',
+    },
+  },
+}
